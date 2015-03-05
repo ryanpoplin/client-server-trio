90a7b0f4b4f6b610bacc6bff8b994fa7805f3283
Byrdann Fox <mobileupstate@gmail.com>
2015-03-05 09:09:42 -0500

    Updates...

--- app/scripts/arrays.js
+++ app/scripts/arrays.js
@@ -4,6 +4,60 @@
         <!-- <script src="scripts/main.js"></script> -->
         <!-- endbuild -->*/
 
+var objOne = {keyOne: 1, keyTwo: 2};
+var objTwo = {keyOne: 1, keyTwo: 2, keyThree: 3};
+
+function compareTwoObjects (one, two) {
+
+	 JSON.stringify(one) === JSON.stringify(two); 
+
+}
+
+compareTwoObjects(objOne, objTwo);
+
+// compare two objects...
+
+// Object.equals = function( x, y ) {
+//   if ( x === y ) return true;
+//     // if both x and y are null or undefined and exactly the same
+
+//   if ( ! ( x instanceof Object ) || ! ( y instanceof Object ) ) return false;
+//     // if they are not strictly equal, they both need to be Objects
+
+//   if ( x.constructor !== y.constructor ) return false;
+//     // they must have the exact same prototype chain, the closest we can do is
+//     // test there constructor.
+
+//   for ( var p in x ) {
+//     if ( ! x.hasOwnProperty( p ) ) continue;
+//       // other properties were tested using x.constructor === y.constructor
+
+//     if ( ! y.hasOwnProperty( p ) ) return false;
+//       // allows to compare x[ p ] and y[ p ] when set to undefined
+
+//     if ( x[ p ] === y[ p ] ) continue;
+//       // if they have the same strict value or identity then they are equal
+
+//     if ( typeof( x[ p ] ) !== "object" ) return false;
+//       // Numbers, Strings, Functions, Booleans must be strictly equal
+
+//     if ( ! Object.equals( x[ p ],  y[ p ] ) ) return false;
+//       // Objects and Arrays must be tested recursively
+//   }
+
+//   for ( p in y ) {
+//     if ( y.hasOwnProperty( p ) && ! x.hasOwnProperty( p ) ) return false;
+//       // allows x[ p ] to be set to undefined
+//   }
+//   return true;
+// }
+
+// palindrome string...
+
+// function checkPalindrom(str) {
+//     return str == str.split('').reverse().join('');
+// }
+
 // var averageTempJan = 31.9;
 // var averageTempFeb = 35.3;
 // var averageTempMar = 42.4;
